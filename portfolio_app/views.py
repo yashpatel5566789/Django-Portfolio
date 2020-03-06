ea3118171c2bff4703565ab14b3ce9295d9a63f5
yashpatel5566789 <yashpatel5566789@gmail.com>
2020-03-06 19:05:25 +0530

    first commit

---
+++ portfolio_app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'index.html', {})
+
+def about(request):
+    return render(request, 'about.html', {})
+
+def contact(request):
+    return render(request, 'contact.html', {})
+
+def work(request):
+    return render(request, 'work.html', {})
